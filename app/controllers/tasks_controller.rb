fed380912ee9554199b8aa94e527fd78ae8c9c31
tatsuya.fujiwara <tatsuya.fujiwara01@gmail.com>
2018-10-13 09:25:52 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,22 +11,13 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
-  def create
-    @task = Task.new(task_params)
-
-    if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
-    else
-      flash.now[:danger] = 'Task が投稿されませんでした'
-      render :new  
-    end
-  end
-
   def edit
     @task = Task.find(params[:id])
   end
-
+  
+  def create
+  end 
+  
   def update
   end
 
